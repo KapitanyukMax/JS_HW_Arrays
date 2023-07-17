6de6b3f1766c2501fc467d445dc55cceec990871
Максим Капітанюк <maxik20192006max@gmail.com>
2023-07-17 16:31:27 +0300

    Task 2

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-Task1();
+//1
+let arr = [];
+for (let i = 0; i < 20; i++)
+    arr.push(Math.floor(Math.random() * 100) + 1);
 
-function Task1() {
-    let arr = [];
-    for (let i = 0; i < 20; i++)
-        arr.push(Math.floor(Math.random() * 100) + 1);
-    
-    console.log(arr);
-}
+//2
+for (const index in arr)
+    console.log(`[${+index + 1}] - ${arr[index]}`);
