93572b278fdf5ee960c26c32492609620e18ffc2
Максим Капітанюк <maxik20192006max@gmail.com>
2023-07-17 17:39:21 +0300

    Tasks 3 and 4

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ for (let i = 0; i < 20; i++)
 //2
 for (const index in arr)
     console.log(`[${+index + 1}] - ${arr[index]}`);
+
+//3
+let multiplesOf7 = arr.filter(elem => elem % 7 === 0);
+multiplesOf7.length === 0 ? console.log("There are no multiples of 7 in the array")
+    : console.log(`Multiples of 7: ${multiplesOf7}`);
+
+//4
+arr.sort((a, b) => b - a);
+console.log(`Ordered by descending array: ${arr}`);
